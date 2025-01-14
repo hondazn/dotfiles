75320c0924d860ec2d7b1ba75389daec7f488a52
HONDA Zyun <hondazn.dev@gmail.com>
2025-01-14 10:35:03 +0900

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,17 +1,16 @@
 if status is-interactive
 	# Commands to run in interactive sessions can go here
-
 	tabs -4
 
+	# CLI tools
+	eval "$(/opt/homebrew/bin/brew shellenv)"
+	eval "$(/opt/homebrew/bin/mise activate fish)"
 	starship init fish | source
-	fzf --fish | source
 
-	# Git
+	# Alias
 	alias gst "git status --short --branch"
 	alias glog "git log --oneline --decorate=short --date=format:'%Y-%m-%d %H:%M:%S' --pretty=format:'%Cgreen%h %C(yellow)%cd %Cred%d %Creset%s %Cblue<%cn>'"
 	alias ggra "git log --graph --oneline --decorate=short --date=format:'%Y-%m-%d %H:%M:%S' --pretty=format:'%Cgreen%h %C(yellow)%cd %Cred%d %Creset%s %Cblue<%cn>'"
 	alias gdifff "git diff --name-only"
 	alias gdiffw "git diff --word-diff"
 end
-
-eval "$(/opt/homebrew/bin/mise activate fish)"
