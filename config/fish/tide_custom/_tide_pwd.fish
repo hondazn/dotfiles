bbbaee075f0cc8205798668c0ee6aae92f02db20
hondazn <hondazn.dev@gmail.com>
2026-04-06 10:06:32 +0900

    update

--- config/fish/tide_custom/_tide_pwd.fish
+++ config/fish/tide_custom/_tide_pwd.fish
@@ -5,12 +5,15 @@ if not functions -q _tide_pwd_original
 end
 
 function _tide_pwd
-    set -l output (_tide_pwd_original)
-    # _tide_pwd_len is already set as global by Tide's function
-
     if set -l _gh_url (_tide_github_url 2>/dev/null)
-        _tide_osc8_wrap "$_gh_url" "$output"
+        set -l output (_tide_pwd_original)
+        # OSC 8 hyperlink opener resets SGR on some terminals; re-apply pwd segment colors.
+        set -l pwd_restore (set_color normal -b $tide_pwd_bg_color; set_color $tide_pwd_color_dirs)
+        printf '\e]8;;%s\e\\' "$_gh_url"
+        echo -ns $pwd_restore$output
+        printf '\e]8;;\e\\'
     else
-        echo -ns $output
+        # Avoid extra command substitution so colors match stock Tide.
+        _tide_pwd_original
     end
 end
